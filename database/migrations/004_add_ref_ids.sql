ccc7bf1d45c3038497925d2a01b1e90ba51478e2
Amanuel Wondimu <amanuelabb@gmail.com>
2026-08-12 12:02:04 +0300

    chore: add database migration for transaction reference IDs (iter 3)

--- database/migrations/004_add_ref_ids.sql
+++ database/migrations/004_add_ref_ids.sql
@@ -4,3 +4,6 @@
 
 # Progress update on 2026-07-19 - chore: add database migration for transaction reference IDs (iter 2)
 # Revision: 20
+
+# Progress update on 2026-08-12 - chore: add database migration for transaction reference IDs (iter 3)
+# Revision: 30
